3d6aa14eefc253e3a6f83f06b2d9090f1391ddeb
L3g3nDAr <erik.vasilenko2002@gmail.com>
2021-07-27 00:06:23 +0300

    add a new function

--- menu/views.py
+++ menu/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.views.generic.edit import CreateView
 from .forms import BbForm
-import requests
 from .models import DB
 
 
@@ -34,10 +33,6 @@ def get_question():
 def start_the_quiz(request):
     question1, question2, question3, correct_answer1, correct_answer2, correct_answer3 = get_question()
     count = 0
-    print(request.POST)
-    print(correct_answer1)
-    print(correct_answer2)
-    print(correct_answer3)
     if request.method == 'POST':
         if request.POST['1'] == str(correct_answer1):
             count += 1
@@ -51,30 +46,15 @@ def start_the_quiz(request):
         return render(request, 'menu/startq.html', {"question1": question1, "question2": question2, "question3": question3})
 
 
-# def start_the_quiz(request):
-# questions = DB.objects.order_by('?')[:3]
-# content = {'questions': questions}
-# if request.method == 'POST':
-#   firstq = questions[0]
-#    secoundq = questions[1]
-#   thirdq = questions[2]
-#    counter = 0
-#    if request.POST['csrfmiddlewaretoken']['0'] == firstq.answer:
-#      counter += 1
-#    if request.POST['csrfmiddlewaretoken']['1'] == secoundq.answer:
-#       counter += 1
-#   if request.POST['csrfmiddlewaretoken']['2'] == thirdq.answer:
-#       counter += 1
-#   return render(request, 'menu/results.html', {'counter': counter})
-# else:
-#   return render(request, 'menu/startq.html', content)
-
 def results(request):
     return render(request, 'menu/results.html')
 
 
 def create(request):
-    return render(request, 'menu/create.html')
+    if request.user.is_authenticated:
+        return render(request, 'menu/create.html')
+    else:
+        return redirect('menu')
 
 
 def register(request):
